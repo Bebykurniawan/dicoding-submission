2367fe85eb2483188a610a880312277b316092a4
Beby Kurniawan <bebykurniawan2006@gmail.com>
2024-02-03 21:26:27 +0700

    hero section added

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
-// const menu = document.querySelector("#hamburger-menu");
+const menu = document.querySelector("#hamburger-menu");
 const navbar = document.querySelector(".navbar");
+const header = document.querySelector(".header");
 document.querySelector('#hamburger-menu').onclick = () => {
     navbar.classList.toggle("active")
+}
+
+document.addEventListener('click',(e) => {
+    if(!menu.contains(e.target) && !menu.contains(e.target)) {
+        navbar.classList.remove("active")
+    }
+})
+
+window.onscroll = function() {
+    if (document.body.scrollTop >= 100 || document.documentElement.scrollTop >= 100 ) {
+        header.classList.add("header-scroll-active") 
+    } else {
+        header.classList.remove("header-scroll-active") 
+        
+    }
+
 }
