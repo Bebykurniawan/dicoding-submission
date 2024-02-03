ec53ac2b04b8e533cb42940189ef17052edeb213
Beby Kurniawan <bebykurniawan2006@gmail.com>
2024-02-03 18:55:48 +0700

    navbar added

---
+++ main.js
@@ -0,0 +1,5 @@
+// const menu = document.querySelector("#hamburger-menu");
+const navbar = document.querySelector(".navbar");
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbar.classList.toggle("active")
+}
